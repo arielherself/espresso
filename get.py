2d9e56fd45b12c5692849784f8c0593c708bf95a
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-15 10:12:47 +0800

    Update get.py

--- get.py
+++ get.py
@@ -43,7 +43,8 @@ def get(proxy=False, http_proxy='', https_proxy=''):
 def web_process(**kwargs):
     pre, titles, details = get(**kwargs)
     mdlines = []
-    mdlines.append(f'# <img src="https://www.economist.com/engassets/ico/favicon.f1ea908894.ico" style="zoom:25%" /> The world in brief {str(datetime.date.today())}')
+    mdlines.append('<div align="center"><img src="https://www.economist.com/engassets/ico/touch-icon-180x180.f1ea908894.png" align-center /></div>')
+    mdlines.append(f'# The world in brief {str(datetime.date.today())}')
     for each in pre:
         if each == '':
             continue
