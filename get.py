d415e75bc0bf76c320998ff5f01b248f141d697e
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-23 12:47:10 +0800

    format optimisation

--- get.py
+++ get.py
@@ -140,7 +140,7 @@ def web_process(**kwargs):
     mdlines.append("*Owing to the difference between time zones of servers in which our auto-update script is running, content above probably doesn't match the one in your region.*")
     
     mdlines_proxied = [line.replace('www.economist.com', MIRROR).replace('economist.com', MIRROR) for line in mdlines]
-    mdlines_proxied.append('|<div align="center"><img src="unlock.png" /><h1>Privacy Information</h1></div></br>We use a certain script to prevent the paywall from loading, thus links contained lead you to the corresponding webpages proxied. \
+    mdlines_proxied.append('|<br><div align="center"><img src="unlock.png" /><h1>Privacy Information</h1></div></br>We use a certain script to prevent the paywall from loading, thus links contained lead you to the corresponding webpages proxied. \
 Visiting these webpages means you have already acknowledged potential risks of having your behaviour recorded by Cloudflare.\
 <br><br>[&#x21BA; Visit the unmodified version](README.raw.md)<br><br>|\n|-----|')
     return mdlines_proxied, mdlines
