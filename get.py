0ecfe357c0b8b23fffe963d8f2a9bfe7c0e85f8b
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-23 12:42:46 +0800

    format optimisation

--- get.py
+++ get.py
@@ -142,7 +142,7 @@ def web_process(**kwargs):
     mdlines_proxied = [line.replace('www.economist.com', MIRROR).replace('economist.com', MIRROR) for line in mdlines]
     mdlines_proxied.append('|<div align="center"><img src="unlock.png" /><h1>Privacy Information</h1></div></br>We use a certain script to prevent the paywall from loading, thus links contained lead you to the corresponding webpages proxied. \
 Visiting these webpages means you have already acknowledged potential risks of having your behaviour recorded by Cloudflare.\
-For an unmodified version, [click here](README.raw.md).|\n|-----|')
+<br><br>[&#x21BA; Visit the unmodified version](README.raw.md)<br><br>|\n|-----|')
     return mdlines_proxied, mdlines
 
 if __name__ == '__main__':
